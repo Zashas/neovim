a79862c019d5efba66bb946b0d507f3d09a3cf86
Mathieu Xhonneux <m.xhonneux@gmail.com>
2017-11-25 12:08:05 +0100

    Update run_tests.sh

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -1,26 +1,3 @@
-#!/usr/bin/env bash
+Coucou
 
-set -e
-set -o pipefail
-
-# TODO: Stop here for MinGW builds,
-#       building Neovim doesn't work yet.
-if [[ "${BUILD_MINGW}" == ON ]]; then
-  echo "Neovim doesn't build on MinGW yet; stopping build."
-  touch "${SUCCESS_MARKER}"
-  exit
-fi
-
-CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "${CI_DIR}/common/build.sh"
-source "${CI_DIR}/common/test.sh"
-
-build_nvim
-
-run_unittests
-run_functionaltests
-run_oldtests
-
-install_nvim
-
-touch "${SUCCESS_MARKER}"
+http://www.pokepedia.fr/images/thumb/1/1b/Pierre.jpg/300px-Pierre.jpg
